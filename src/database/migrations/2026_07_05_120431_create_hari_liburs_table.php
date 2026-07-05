bde1092964a41ade7057553f82585a38bac742c1
illhammf <ilhamfrafli@gmail.com>
2026-07-05 12:21:50 +0700

    update migration hari libur

--- src/database/migrations/2026_07_05_120431_create_hari_liburs_table.php
+++ src/database/migrations/2026_07_05_120431_create_hari_liburs_table.php
@@ -13,7 +13,31 @@ return new class extends Migration
     {
         Schema::create('hari_liburs', function (Blueprint $table) {
             $table->id();
+
+            $table->string('nama_hari_libur');
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai')->nullable();
+
+            $table->enum('jenis', [
+                'nasional',
+                'operasional',
+                'lainnya',
+            ])->default('operasional');
+
+            $table->text('keterangan')->nullable();
+
+            $table->enum('status', [
+                'aktif',
+                'nonaktif',
+            ])->default('aktif');
+
             $table->timestamps();
+
+            $table->index('nama_hari_libur');
+            $table->index('tanggal_mulai');
+            $table->index('tanggal_selesai');
+            $table->index('jenis');
+            $table->index('status');
         });
     }
 
