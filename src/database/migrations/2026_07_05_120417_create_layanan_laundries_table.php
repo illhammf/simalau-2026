f337f8094aa0a5e163732ece29d5e1e8cbf54b63
illhammf <ilhamfrafli@gmail.com>
2026-07-05 12:11:40 +0700

    update migration layanan

--- src/database/migrations/2026_07_05_120417_create_layanan_laundries_table.php
+++ src/database/migrations/2026_07_05_120417_create_layanan_laundries_table.php
@@ -13,7 +13,31 @@ return new class extends Migration
     {
         Schema::create('layanan_laundries', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('kategori_layanan_id')
+                ->constrained('kategori_layanans')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table->string('nama_layanan');
+            $table->string('slug')->unique();
+            $table->text('deskripsi')->nullable();
+
+            $table->enum('tipe_layanan', ['kiloan', 'satuan']);
+            $table->decimal('tarif', 12, 2)->default(0);
+
+            $table->integer('estimasi_hari')->default(1);
+            $table->integer('minimal_order')->nullable();
+
+            $table->string('satuan_hitung')->default('kg');
+            $table->enum('status', ['aktif', 'nonaktif'])->default('aktif');
+
             $table->timestamps();
+
+            $table->index('kategori_layanan_id');
+            $table->index('nama_layanan');
+            $table->index('tipe_layanan');
+            $table->index('status');
         });
     }
 
