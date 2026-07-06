c497288f054c774c55d89d49f352f92bc8678bbf
illhammf <ilhamfrafli@gmail.com>
2026-07-06 10:20:18 +0700

    update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\LayananController;
+use App\Http\Controllers\Front\PesananController;
+use App\Http\Controllers\Front\RiwayatPesananController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
-use Illuminate\Support\Facades\Response;
 
 /* NOTE: Do Not Remove
 / Livewire asset handling if using sub folder in domain
@@ -18,6 +21,26 @@ Livewire::setScriptRoute(function ($handle) {
 /*
 / END
 */
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [HomeController::class, 'index'])
+    ->name('front.home');
+
+Route::get('/layanan', [LayananController::class, 'index'])
+    ->name('front.layanan.index');
+
+Route::get('/layanan/{slug}', [LayananController::class, 'show'])
+    ->name('front.layanan.show');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/pesanan', [RiwayatPesananController::class, 'index'])
+        ->name('front.pesanan.index');
+
+    Route::get('/pesanan/buat', [PesananController::class, 'create'])
+        ->name('front.pesanan.create');
+
+    Route::post('/pesanan', [PesananController::class, 'store'])
+        ->name('front.pesanan.store');
+
+    Route::get('/pesanan/{nomor_pesanan}', [RiwayatPesananController::class, 'show'])
+        ->name('front.pesanan.show');
 });
